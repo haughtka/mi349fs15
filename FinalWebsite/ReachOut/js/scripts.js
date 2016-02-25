0f3e2b696668b3f54a386fd0cf60c7d2b883c05c
haughtka <haughtka@msu.edu>
2016-02-24 19:59:08 -0500

    FinalProject

---
+++ FinalWebsite/ReachOut/js/scripts.js
@@ -0,0 +1,29 @@
+
+function slideSwitch() {
+    var $active = $('#slideshow IMG.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+
+    $active.addClass('last-active');
+        
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 3000 );
+});
+
+$(function(){
+    $('#nav').hover(function(){
+        $(this).animate({width:'200px'},500);
+    },function(){
+        $(this).animate({width:'20px'},500);
+    }).trigger('mouseleave');
+});
